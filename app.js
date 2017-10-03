5e3f0d2a686118c6f682255874c3bf42fde44642
Lucyloules <lucia.lagos@gmail.com>
2017-10-03 18:09:52 -0300

    Estructura HTML y JS

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+// Mediante prompt le pediremos al usuario que ingrese una frase
+var cadena = prompt("Ingrese la frase");
+// la funcion cifradoCesar codifica la frase ingresada por usuario
+var cifradoCesar= function(str){
+	return str
+}
+document.write(cadena);
+//document.write((cifradoCesar(str));
+//String.fromCharCode(65,66,67) --> 'A', 'B', 'C' en codigo ascii
